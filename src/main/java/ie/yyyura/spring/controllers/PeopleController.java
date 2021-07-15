f29ae898b956743dd02f5c06f52d080916e06956
Jurijs Cicelimovs <yyyura@gmail.com>
2021-07-15 01:11:28 +0100

    AbstractAnnotationConfigDispatcherServletInitializer

---
+++ src/main/java/ie/yyyura/spring/controllers/PeopleController.java
@@ -0,0 +1,28 @@
+package ie.yyyura.spring.controllers;
+
+import ie.yyyura.spring.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String all(Model model){
+
+        model.addAttribute("allPeople", personDAO.all());
+        return "people/all";
+    }
+
+}
---
+++ src/main/java/ie/yyyura/spring/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package ie.yyyura.spring.dao;
+
+import ie.yyyura.spring.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int COUNT;
+    private List<Person> peopleList;
+
+    //initialization block
+    {
+        peopleList = new ArrayList<>();
+        //public Person(int id, String name)
+        peopleList.add(new Person(++COUNT, "Tom"));
+        peopleList.add(new Person(++COUNT, "Bom"));
+        peopleList.add(new Person(++COUNT, "Pom"));
+    }
+
+    //return all
+    public List<Person> all() {
+        return peopleList;
+    }
+
+    //get one with id
+    public Person one(int idIN) {
+        return peopleList.stream().filter(p -> p.getId() == idIN).findAny().orElse(null);
+    }
+
+}
