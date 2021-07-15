1214197c35bc172bd986cd7f8d06665056489f7d
Jurijs Cicelimovs <yyyura@gmail.com>
2021-07-15 09:09:27 +0100

    @PathVariable, @RequestParam

--- src/main/java/ie/yyyura/spring/controllers/PeopleController.java
+++ src/main/java/ie/yyyura/spring/controllers/PeopleController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,9 +21,14 @@ public class PeopleController {
 
     @GetMapping()
     public String all(Model model){
-
         model.addAttribute("allPeople", personDAO.all());
         return "people/all";
+    }
+
+    @GetMapping("/{id}")
+    public String one(@PathVariable("id") int idIN, Model model){
+        model.addAttribute("onePerson", personDAO.one(idIN));
+        return "people/one";
     }
 
 }
